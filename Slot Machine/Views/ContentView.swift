19bb726f1c911c4a697fb89368db5a5d7ea62f65
Reinaldo <reinaldo.riant@kompas.com>
2021-07-19 05:47:20 +0700

    + add header

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            //MARK : - Background
+            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            //MARK : - Interface
+            VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5){
+                
+                //MARK : - Header
+                LogoView()
+                Spacer()
+                //MARK : - Score
+                //MARK : - Slot Machine
+                //MARK : - Footer
+            }
+            //MARK : - Buttons
+            .overlay(
+                Button(action: {
+                    print("Reset the game")
+                }) {
+                    Image(systemName: "arrow.2.circlepath.circle")
+                }
+                .modifier(ButtonModifier()),alignment: .topLeading
+            )
+            .overlay(
+                Button(action: {
+                    print("Reset the game")
+                }) {
+                    Image(systemName: "info.circle")
+                }
+                .modifier(ButtonModifier()),alignment: .topTrailing
+            )
             .padding()
+            .frame(maxWidth: 720)
+        }
     }
 }
 
